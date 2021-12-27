1288f1a2cfee4148a584f193e7eed3c3ba14a7f8
eyujunior <eyujunior052@gmail.com>
2021-12-27 13:08:37 -0800

    first react component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      title: "Car insurance",
+      amount: 287.8,
+      date: new Date(2000, 3, 28),
+    },
+    {
+      title: "Soccer Betting",
+      amount: 1000,
+      date: new Date(2019, 4, 22),
+    },
+    {
+      title: "Cristiano Ronaldo",
+      amount: 778.2,
+      date: new Date(2003, 2, 2),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,20 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
-    <div>
-      <p>Date</p>
-      <h2>Title</h2>
-      <p>Amount</p>
+    <div className="expense-item">
+      <div>
+        <p>{month}</p>
+        <p>{day}</p>
+        <p>{year}</p>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <p className="expense-item__price">${props.amount}</p>
+      </div>
     </div>
   );
 }
